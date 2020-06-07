d56006c6d7a713105f1be38180cdc0a7a771589d
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-06-07 09:24:29 +0530

    first error

---
+++ src/components/streams/StreamDelete.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { connect } from 'react-redux';
+
+export const StreamDelete = () => {
+  return <div>StreamDelete</div>;
+};
+
+//const mapStateToProps = (state) => ({});
+
+//const mapDispatchToProps = {};
+
+export default StreamDelete;
---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//import { connect } from 'react-redux';
+
+export const StreamShow = () => {
+  return <div>StreamShow</div>;
+};
+
+//const mapStateToProps = (state) => ({});
+
+//const mapDispatchToProps = {};
+
+export default StreamShow;
